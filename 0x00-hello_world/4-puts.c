45b1cdbb91dfa23b1af346f3247a7e2e7c3ff7f4
Assalet <assaletkoua@gmail.com>
2022-03-13 23:17:44 +0000

    task 4 - puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 /**
- *Use the function puts
+ * main - Entry point
  *
- *return :0
+ * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
