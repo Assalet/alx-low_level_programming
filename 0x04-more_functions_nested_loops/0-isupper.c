0f9f2b1f133a6bf1be12ab04f69f4171f52c3bde
Assalet <assaletkoua@gmail.com>
2022-03-17 23:57:41 +0000

    Task 1 - isuppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper : function to check for uppercase character.
+ *
+ * Return : 1 if uppercase or 0 if not.
+ *
+ * @c : a character in ASCI code.
+ */
+
+int _isupper(int c)
+{
+	if (c <= 90 && c >= 65)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
