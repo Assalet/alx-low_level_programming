15cfcb485624af4c28bcf71b5449a8103736729d
Assalet <assaletkoua@gmail.com>
2022-03-18 00:22:53 +0000

    Task 1 - isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isdigit - check for a digit
+ *
+ * Return: 1 if c is a digit, 0 if not
+ *
+ *@c: ASCI character code
+ */
+int _isdigit(int c)
+{
+	if (c <= 59 && c >= 48)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
