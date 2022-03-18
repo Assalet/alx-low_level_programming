8054a7c939364d8ea8a38bac6aae955108b2d1af
Assalet <assaletkoua@gmail.com>
2022-03-18 01:00:42 +0000

    task 2 - multiplication

---
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ *
+ */
+int main(void)
+{
+	printf("%d \n", mul(12, 4));
+	printf("%d \n", mul(-10, -10));
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply two intergers.
+ *
+ * Return: multiplication between two integers.
+ * @a: is an interger.
+ * @b: is an integer.
+ */
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
