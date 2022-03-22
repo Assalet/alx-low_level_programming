7a2e3800fd658509fb01020fe9a3c67ca61b95ad
Assalet <assaletkoua@gmail.com>
2022-03-22 21:48:09 +0000

    task 0 - reset numbers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update a value of a pointer to a variable it points
+ * @n: is a pointer
+ * Return: 98.
+ */
+int reset_to_98(int *n)
+{
+	*n = 98;
+	return (0);
+}
