bb1fda99ce528352491d8638055aaa6530d6b63e
Assalet <assaletkoua@gmail.com>
2022-03-22 23:54:25 +0000

    task 1 - swap two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * swap_int - swap the values of two integers
+ *
+ *@a: is an integer
+ *@b: is an integer
+ *
+ * Return: 0
+ */
+int swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+	return (0);
+}
